68bc5130016b3c5c4c84f2897295b03f2e5a2add
ksh981214 <ksh981214@korea.ac.kr>
2018-06-05 22:20:29 +0900

    shinhwan

--- templates/index.php
+++ templates/index.php
@@ -1,50 +1,44 @@
 <?php
-      if(is_uploaded_file($_FILES['file']['tmp_name'])) {
+     if(is_uploaded_file($_FILES['file']['tmp_name'])) {
 
           function file_upload($file, $folder, $allowExt, $file_name) {
-                $ext = substr(strrchr($file['name'], '.'), 1);
-  
-                if($ext) {
-                     $allow = explode(',', $allowExt);
-  
-                     if(is_array($allow)) {
-                          $check = in_array($ext, $allow);
-                     } else {
-                          $check = ($ext == $allow) ? true : false;
-                     }
-                }
-  
-                $upload_file =  $folder.$file_name;
-  
-                if(@move_uploaded_file($file['tmp_name'], $upload_file)) {
-                     @chmod($upload_file, 0707);
-                        $return = ' type="text/javascript">function copy(str) { clipboardData.setData("Text", str); "°æ·Î°¡ º¹»ç µÊ"); }
-                        </script>';
-                        $return.= '¾÷·Îµå µÈ ÆÄÀÏ °æ·Î : . <a href="'.$upload_file.'">'.$upload_file.'</a> ';
-                        return $return;
+               $ext = substr(strrchr($file['name'], '.'), 1);
+ 
+               if($ext) {
+                    $allow = explode(',', $allowExt);
+ 
+                    if(is_array($allow)) {
+                         $check = in_array($ext, $allow);
+                    } else {
+                         $check = ($ext == $allow) ? true : false;
+                    }
+               }
+ 
+               $upload_file =  $folder.$file_name;
+ 
+               if(@move_uploaded_file($file['tmp_name'], $upload_file)) {
+                    @chmod($upload_file, 0707);
+$return = '<script type="text/javascript">function copy(str) { clipboardData.setData("Text", str); alert("경로가 복사 됨"); }
+</script>';
+$return.= '업로드 된 파일 경로 : . <a href="'.$upload_file.'">'.$upload_file.'</a> ';
+return $return;
 
-                                    } else {
-                                          exit(' type="text/javascript">\'¾÷·Îµå¿¡ ½ÇÆÐÇÏ¿´½À´Ï´Ù!\'); history.go(-1);</script>');
-                                    }
-                              }
-                        
+               } else {
+                    exit('<script type="text/javascript">alert(\'업로드에 실패하였습니다!\'); history.go(-1);</script>');
+               }
+          }
+ 
 
-                              $upload_return = file_upload($_FILES['file'], 'upload/', 'jpg,gif,png,jpeg,JPG,GIF,PNG,JPEG', $_FILES['file']['name']);
-  
-           echo $upload_return;
-  
-      } else {
- ?>
-
- <script>
-       
-       var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
-       alert(newURL);
-       </script>
- <form action="index.php" method="post" enctype="multipart/form-data">
-      <input type="file" name="file" /><br />
-      <input type="submit" value="Upload" />
- </form>
- <?php
-      }
- ?>
+          $upload_return = file_upload($_FILES['file'], 'upload/', 'jpg,gif,png,jpeg,JPG,GIF,PNG,JPEG', $_FILES['file']['name']);
+ 
+          echo $upload_return;
+ 
+     } else {
+?>
+<form action="index.php" method="post" enctype="multipart/form-data">
+     <input type="file" name="file" /><br />
+     <input type="submit" value="Upload" />
+</form>
+<?php
+     }
+?>
