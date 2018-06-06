a66c3c10519f0a1ec54440b58f290bc473afd8cc
ksh981214 <ksh981214@korea.ac.kr>
2018-06-06 16:16:00 +0900

    abc

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ def upload_file():
   vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
   vision_analyze_url = vision_base_url + "analyze"
 
+
   headers = {'Ocp-Apim-Subscription-Key': subscription_key }
   params = {'visualFeatures': 'Categories,Description,Color'}
-  data = {'url': 'uploads/'+file.filename}
-  response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
+  data = {'url': f}
+  response = requests.post(vision_analyze_url, headers=headers, params=params, data=image_data)
   response.raise_for_status()
 
   analysis = response.json()
