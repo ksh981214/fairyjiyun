d0dbbce85dddeae5987a849d56183c120211db68
ksh981214 <ksh981214@korea.ac.kr>
2018-06-06 16:19:43 +0900

    Abc

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ assert subscription_key
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = os.path.basename('uploads')
+UPLOAD_FOLDER = os.path.basename('/uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-  #return image_caption
-  return render_template('index.html')
+  vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+  vision_analyze_url = vision_base_url + "analyze"
 
+  image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
+    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+
+  headers = {'Ocp-Apim-Subscription-Key': subscription_key }
+  params = {'visualFeatures': 'Categories,Description,Color'}
+  data = {'url': image_url}
+  response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
+  response.raise_for_status()
+
+  analysis = response.json()
+  image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+
+  return render_template('index.html', image_caption=image_caption)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -26,15 +39,15 @@ def upload_file():
   f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
   file.save(f)
-  
+  print(f)
+
   vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
   vision_analyze_url = vision_base_url + "analyze"
-
 
   headers = {'Ocp-Apim-Subscription-Key': subscription_key }
   params = {'visualFeatures': 'Categories,Description,Color'}
   data = {'url': f}
-  response = requests.post(vision_analyze_url, headers=headers, params=params, data=image_data)
+  response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
   response.raise_for_status()
 
   analysis = response.json()
